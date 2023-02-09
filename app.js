893b8acb7b4fde682bab9c93a5dc5a2d4d08e23a
陈健 <5414779+chen_jian_xiangxi@user.noreply.gitee.com>
2023-02-09 08:51:36 +0800

    中间件

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ const express = require('express')
 // 创建 express 应用
 const app = express()
 
+//中间件必须在请求发起前响应
+// function myLogger(req,res,next){
+//     console.log('myLogger')
+//     // next()  // 必须添加，否则不执行后面的
+// }
+//
+// app.use(myLogger)
+
 // 监听 / 路径的 get 请求
-app.get('/', function(req, res) {
-    res.send('hello node')
-})
+
 
 // 使 express 监听 5000 端口号发起的 http 请求
 const server = app.listen(5000, function() {

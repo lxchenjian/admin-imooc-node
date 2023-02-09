c4409835259a551d29c62172049b52070edd9524
陈健 <5414779+chen_jian_xiangxi@user.noreply.gitee.com>
2023-02-09 08:58:09 +0800

    路由的引入 报错是运行不了

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # 基础命令
-
+## 初始化命令
+npm init -y
+npm i -S express
+创建app.js
 node app.js 启动
+
+## 路由
+npm i -S boom
+创建 router/index.js
+
 
 # 找不到的问题
 
--- app.js
+++ app.js
@@ -1,18 +1,10 @@
 const express = require('express')
+const router = require('./router')
 
 // 创建 express 应用
 const app = express()
 
-//中间件必须在请求发起前响应
-// function myLogger(req,res,next){
-//     console.log('myLogger')
-//     // next()  // 必须添加，否则不执行后面的
-// }
-//
-// app.use(myLogger)
-
-// 监听 / 路径的 get 请求
-
+app.use('/',router)
 
 // 使 express 监听 5000 端口号发起的 http 请求
 const server = app.listen(5000, function() {
